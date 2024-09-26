575ecf064c1fb3d3413acef300447fa733c829b7
Brian Merrill <bmerrill@proofpoint.com>
2024-09-25 21:29:01 -0700

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+import UpscaleSlicer
+NODE_CLASS_MAPPINGS = { "Upscale Slicer":UpscaleSlicer }
+NODE_DISPLAY_NAME_MAPPINGS = { "Upscale Slicer" : "Upscale Slicer"}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
