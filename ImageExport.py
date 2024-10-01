bf9d0c401325de057687dca0cbac6da5010f1397
Brian Merrill <bmerrill@proofpoint.com>
2024-09-30 23:12:04 -0700

    Add node to base64 encode and image and export it to a text file.

---
+++ ImageExport.py
@@ -0,0 +1,46 @@
+import base64
+import io
+import os
+from PIL import Image
+
+class ImageExport:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "output_directory": ("STRING", {"default": os.environ.get("COMFYUI_OUTPUT_PATH")}),
+                "filename": ("STRING", {"default": "encoded_image.txt"})
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("encoded_image",)
+    FUNCTION = "encode_image"
+    CATEGORY = "Liberated Nodes/Image"
+
+    def encode_image(self, image: Image.Image, output_directory: str, filename: str):
+        # Convert image to bytes
+        buffered = io.BytesIO()
+        image.save(buffered, format=image.format)
+        image_bytes = buffered.getvalue()
+
+        # Encode bytes to base64
+        image_base64: str = base64.b64encode(image_bytes).decode('utf-8')
+
+        os.makedirs(output_directory, exist_ok=True)
+        filepath = os.path.join(output_directory, filename)
+
+        # Ensure the filename ends with .csv
+        if not filepath.lower().endswith('.txt'):
+            filepath += '.txt'
+
+        # Write base64 string to file
+        try:
+            with open(filepath, "w") as file:
+                file.write(image_base64)
+            print(f"Encoded image saved to {filepath}")
+        except Exception as e:
+            print(f"Error saving encoded image file: {str(e)}")
+
+        return ()
--- __init__.py
+++ __init__.py
@@ -1,4 +1,13 @@
 from .UpscaleSlicer import UpscaleSlicer
-NODE_CLASS_MAPPINGS = { "Upscale Slicer" : UpscaleSlicer }
-NODE_DISPLAY_NAME_MAPPINGS = { "Upscale Slicer" : "Upscale Slicer"}
+from .ImageExport import ImageExport
+
+NODE_CLASS_MAPPINGS = {
+    "UpscaleSlicer": UpscaleSlicer,
+    "ImageExport": ImageExport
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "UpscaleSlicer": "Upscale Slicer",
+    "ImageExport":"Image Export"
+}
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
